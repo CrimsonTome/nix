74f556700646aa4da2ceca31eb0650b9eb033338
CTome <64846840+CrimsonTome@users.noreply.github.com>
2024-02-26 11:52:48 +0000

    install script from docs

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# FOR BIOS SYSTEMS FOR NOW
+
+parted /dev/sda -- mklabel msdos
+parted /dev/sda -- mkpart primary 1MB -8GB
+parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mount /dev/disk/by-label/nixos /mnt
+nixos-generate-config --root /mnt
+nano /mnt/etc/nixos/configuration.nix
+nixos-install
+reboot
